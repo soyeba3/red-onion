ccf016270f43e2d6f60d5e587915b8a77db497d2
soyeba3 <soyebchandani@gmail.com>
2022-02-22 00:31:55 +0600

    done landing page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,78 +1,103 @@
 import React, { useState } from 'react';
-import image1 from '../../data/Image/adult-blur-blurred-background-687824.png'
-import image2 from '../../data/Image/chef-cook-food-33614.png'
-import image3 from '../../data/Image/architecture-building-city-2047397.png'
-import trainIcon from '../../data/ICON/Group 204.png'
-import  bellIcon from '../../data/ICON/Group 1133.png'
-import carIcon from '../../data/ICON/Group 245.png'
+import image1 from '../../data/Image/adult-blur-blurred-background-687824.png';
+import image2 from '../../data/Image/chef-cook-food-33614.png';
+import image3 from '../../data/Image/architecture-building-city-2047397.png';
+import icon1 from '../../data/ICON/Group 204.png';
+import icon3 from '../../data/ICON/Group 245.png';
+import icon2 from '../../data/ICON/Group 1133.png';
+import {faArrowAltCircleRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Footer = () => {
-    const [readMore, setRedMore] =useState(false)
 
-    const readMoreBtn = () => {
-        setRedMore(true)
+
+const Blog = () => {
+
+    const [card1, setCard1] = useState(false);
+    const [card2, setCard2] = useState(false);
+    const [card3, setCard3] = useState(false);
+
+    const buttonClick = (e) => {
+
+        if(e.target.name === 'card1'){
+            !card1 ? setCard1(true) : setCard1(false);
+        }
+        else if(e.target.name === 'card2'){
+            !card2 ? setCard2(true) : setCard2(false); 
+        }
+        else{
+            !card3 ? setCard3(true) : setCard3(false);
+        }
     }
-    const readLessBtn = () => {
-        setRedMore(false)
-    }
-    
+
     return (
-        <div className='py-5 mb-8'>
-            <div className='w-[500px] ml-[100px]'>
-                <h1 className='text-[32px] font-bold m-2'>Why You Choose Us</h1>
-                <p>El Diablo’s cook top is nothing short of extraordinary. Located on a Spanish Island, this restaurant cooks its 
-                    meals over an active volcanic hole in the ground.</p>
+        <div className='flex flex-col'>
+
+            
+            <div className='w-3/5 px-32 pb-8'>
+                <h1 className='font-semibold text-3xl pb-5'>Why you choose us</h1>
+                <p>El Diablo’s cook top is nothing short of extraordinary. Located on a Spanish Island, this restaurant cooks its meals over an active volcanic hole in the ground.</p>
             </div>
-            <div className='flex justify-center items-center mx-20'>
-                <div className='px-4 py-4'>
-                    <div className='mb-2'>
-                        <img className='' src={image1} alt="" />
+
+            <div className='flex flex-row px-[7%] mt-4 justify-between pb-10 '>
+
+
+                <div className='w-[30%] flex flex-col rounded-xl hover:scale-105 duration-300 hover:drop-shadow-xl h-min bg-white'>
+                    <img src={image1} alt="" />
+
+                    <div className='flex flex-row p-5'>
+                        <div className='text-center w-[20%] mt-4'>
+                            <img src={icon1} className='' alt="icon1" />
+                        </div>
+                        <div className='w-[80%] mt-4 pr-5 flex flex-col flex-wrap items-start'>
+                            <h1 className='font-bold'>Fast Delivery</h1>
+                            <div className=''>
+                                <p className=''>For a memorable meal the quality of the service is {!card1 && <span>...</span>}{card1 && <span>something that guests often remember. Restaurant provides a physical good (prepared food), but also provides services in the form of ambience, the setting and clearing of the table, etc.</span>}
+                                </p>
+                            </div>
+                            <button onClick={buttonClick} name='card1' className='text-blue-600 font-semibold'>{card1? 'Show less' : 'Show more'} <FontAwesomeIcon className='text-green-500' icon={faArrowAltCircleRight}/></button>
+                        </div>
                     </div>
-                    <div className='flex items-center'>
-                        <img className='h-8 m-4' src={trainIcon} alt="icon" />
-                        <h2 className='font-Montserrat font-bold text-lg'>Fast Delivery</h2>
-                    </div>
-                    
-                        <p className='mx-4 my-1'>For a memorable meal the quality of the service is something that guests often {
-                            !readMore && <span>...</span>
-                        }</p>
-                        {
-                            readMore &&  <span>remember. Restaurant provides a physical good (prepared food), but also provides services in the form of ambience, the setting and clearing of the table, etc.</span>  
-                        }
-                    
-                   {
-                       readMore ?
-                        <button onClick={readLessBtn} className='text-[#F91944] mx-2 cursor-pointer block'>Read less</button>
-                       : <button onClick={readMoreBtn}  className='text-[#F91944] mx-2 cursor-pointer block'>Read more</button>
-                   }
                 </div>
-                <div className='px-4'>
-                    <div>
-                        <img src={image2} alt="" />
-                    </div> 
-                    <div className='flex items-center'>
-                        <img className='h-8 m-4' src={bellIcon} alt="icon" />
-                        <h2 className='font-Montserrat font-bold text-lg'>A Good Auto Responder</h2>    
-                    </div> 
-                    <p className='mx-4 my-1'>For a memorable meal the quality of the service is something that guests often</p>
-                    <button className='text-[#F91944] mx-4'>Read more...</button> 
-                </div>
-                <div className='px-4'>
-                    <div>
-                        <img src={image3} alt="" />
+                
+                <div className='w-[30%] flex-col rounded-xl hover:scale-105 duration-300 hover:drop-shadow-xl h-min bg-white'>
+                    <img src={image2} alt="" />
+
+                    <div className='flex flex-row p-5'>
+                        <div className='text-center w-[20%] mt-4'>
+                            <img src={icon2} className='' alt="icon2" />
+                        </div>
+                        <div className='w-[80%] mt-4 pr-5 flex flex-col flex-wrap items-start'>
+                            <h1 className='font-bold'>Fast Delivery</h1>
+                            <div className=''>
+                                <p className=''>For a memorable meal the quality of the service is{!card2 && <span>...</span>} {card2 && <span>something that guests often remember. Restaurant provides a physical good (prepared food), but also provides services in the form of ambience, the setting and clearing of the table, etc.</span>}
+                                </p>
+                            </div>
+                            <button onClick={buttonClick} name="card2" className='text-blue-600 font-semibold'>{card2? 'Show less' : 'Show more'} <FontAwesomeIcon className='text-green-500' icon={faArrowAltCircleRight}/></button>
+                        </div>
                     </div>
-                    <div className='flex items-center'>
-                        <img className='h-8 m-4' src={carIcon} alt="icon" />
-                        <h2 className='font-Montserrat font-bold text-lg'>A Good Auto Responder</h2>    
-                    </div> 
-                    <p className='mx-4 my-1'>For a memorable meal the quality of the service is something that guests often</p>
-                    
-                    <button className='text-[#F91944] mx-4'>Read more...</button> 
                 </div>
 
+                <div className='w-[30%] flex-col rounded-xl hover:scale-105 duration-300 hover:drop-shadow-xl h-min bg-white'>
+
+                    <img src={image3} alt="" />
+
+                    <div className='flex flex-row p-5'>
+                        <div className='text-center w-[20%] mt-4'>
+                            <img src={icon3} className='' alt="icon3" />
+                        </div>
+                        <div className='w-[80%] mt-4 pr-5 flex flex-col flex-wrap items-start'>
+                            <h1 className='font-bold'>Fast Delivery</h1>
+                            <div className=' '>
+                                <p className=''>For a memorable meal the quality of the service is{!card3 && <span>...</span>} {card3 && <span>something that guests often remember. Restaurant provides a physical good (prepared food), but also provides services in the form of ambience, the setting and clearing of the table, etc.</span>}
+                                </p>
+                            </div>
+                            <button onClick={buttonClick} name="card3" className='text-blue-600 font-semibold'>{card3? 'Show less' : 'Show more'} <FontAwesomeIcon className='text-green-500' icon={faArrowAltCircleRight}/></button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
-export default Footer;
+export default Blog;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,10 +12,10 @@ const Navbar = () => {
             <img className="w-40 sm:w-24" src={logo} alt="" />
           </a>
         </div>
-        <div className="w-70 sm:w-[240px] flex justify-center items-center p-8 sm:p-0 sm:mr-2">
-              <div className="w-16 flex justify-center items-center  mr-4 sm:m-0 hover:border border-[#F91944] 
-              duration-200 ease-linear py-2 rounded-[27px] sm:rounded-[20px]">
-                  <a href="#cart">
+        <div className="w-70 sm:w-[240px] flex justify-center sm:justify-end items-center p-8 sm:p-0 sm:mr-1">
+              <div >
+                  <a className="w-16 sm:w-12 flex justify-center items-center mr-4 sm:m-0 hover:border border-[#F91944] 
+              duration-200 ease-linear py-2 rounded-[27px] sm:rounded-[15px]" href="#cart">
                     <FontAwesomeIcon
                     className="text-[#F91944] h-4 mr-1"
                     icon={faCartShopping}
@@ -23,12 +23,13 @@ const Navbar = () => {
                     <sup className="font-bold text-base text-[#F91944]">0</sup>
                   </a>
               </div>
-              <div className="duration-200 ease-linear font-Montserrat sm:font-normal m-4 sm:m-1 text-lg sm:text-base hover:border
-                           border-[#F91944] py-1 sm:py-1 px-4 sm:px-3 rounded-[27px] sm:rounded-[20px]">
-                    <a href="#login">Login</a>
+              <div >
+                    <a className="duration-200 ease-linear font-Montserrat sm:font-normal m-4 sm:m-0 text-lg sm:text-base hover:border
+                           border-[#F91944] py-2 sm:py-2 px-4 sm:px-2 rounded-[27px] sm:rounded-[20px]" href="#login">Login</a>
               </div>
-              <div className="duration-200 ease-linear sm:w-[100px] font-Montserrat m-4 sm:m-1 text-base sm:text-base bg-[#F91944] rounded-[27px] sm:rounded-[20px] py-1 sm:py-1 px-4 sm:px-3 text-white">
-                  <a href="#signup">Sign up</a>
+              <div >
+                  <a className="duration-200 ease-linear sm:w-[100px] font-Montserrat m-4 sm:m-0 
+                              text-base sm:text-base bg-[#F91944] rounded-[27px] sm:rounded-[20px] py-2 sm:py-2 px-4 text-white" href="#signup">Sign up</a>
               </div>
         </div>
       </div>
